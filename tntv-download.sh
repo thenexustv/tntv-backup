6bcf7c9d66a4c490d37a6f606a67a489c2f65a23
Ryan Rampersad <ryan.rampersad@gmail.com>
2013-01-26 17:41:08 -0600

    Added gitingore, comments and arguments to tntv-download.sh

--- tntv-download.sh
+++ tntv-download.sh
@@ -16,25 +16,35 @@ function getseries(){
 function getpodcast(){
     if [[ ! -e "$1$2.mp3" ]] ; then
         sleep 2  # just to be nice
-        wget "$urlbase$1/$1$2.mp3"
+        wget -P "podcasts/" "$urlbase$1/$1$2.mp3"
     fi
 }
 
+# Each series is downloaded below
+# After each series is downloaded,
+# exceptions to the series conventions are downloaded
+# ---
 
+# Control Structure
 getseries cs
 getpodcast cs 0 # i'm special!
 
+# Eight Bit
 getseries eb
 
+# The Universe
 getseries tu
 getpodcast tu 18x
 
+# The Fringe
 getseries tf
 getpodcast tf 1x
 
+# Nexus Special
 getseries ns
 getpodcast ns 5x
 
+# At The Nexus
 getseries atn
 getpodcast atn 20x
 getpodcast atn 31x
